05803e9d07d2babd8d80ecde43cf9822b8a60578
George, Le Van Thong <thonglevan131@gmail.com>
2023-05-14 10:35:47 +0700

    confg: enable lua, add custom plugin TODO comment

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,5 @@
+-- TODO: test todo comments
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,10 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    opts = {},
+    event = "User AstroFile",
+  },
 }
